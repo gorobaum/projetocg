6dc9ea03d22968129491d3e56033d56c4e1254b4
Thiago Nunes <gorobaum@gmail.com>
2014-11-11 09:12:33 -0200

    Read height

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,8 @@
 
 int main(int argc, char** argv) 
 {
-	FreeImage_Initialise();
 	DLL_API void DLL_CALLCONV FreeImage_Initialise(BOOL load_local_plugins_only FI_DEFAULT(FALSE));
-	std::cout << FreeImage_GetVersion();
+	std::cout << FreeImage_GetVersion() << "\n";
 	FIBITMAP *bitmap = FreeImage_AllocateT(FIT_RGBAF, 320, 240);
 	if (bitmap) 
 	{
@@ -25,6 +24,7 @@ int main(int argc, char** argv)
 	{
 		FreeImage_Unload(bitmap3);
 	}
+	std::cout << "Has pixeis " << FreeImage_GetHeight(bitmap) << "\n";
 	DLL_API void DLL_CALLCONV FreeImage_DeInitialise();
 	return 0;
 }
