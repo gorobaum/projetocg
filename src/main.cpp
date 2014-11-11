4aa446e34f4092a264a416b474a1b024da91a611
Thiago Nunes <gorobaum@gmail.com>
2014-11-11 17:22:47 -0200

    Testing linear interpolation

--- src/main.cpp
+++ src/main.cpp
@@ -5,41 +5,28 @@
 
 #include <FreeImage.h>
 
-#include "gaussinterpolator.h"
+#include "linearinterpolator.h"
 #include "hdrimage.h"
 
 int main(int argc, char** argv) 
 {
 	DLL_API void DLL_CALLCONV FreeImage_Initialise(BOOL load_local_plugins_only FI_DEFAULT(FALSE));
-
-	std::cout << FreeImage_GetVersion() << "\n";
-	HdrImage teste("lpbom.hdr");
-	std::cout << "Color in [0,0] = " << teste.getPixelAt(0,0).red << "\n";
 	
+	HdrImage one("blue.hdr");
+	HdrImage two("red.hdr");
 
-	FIBITMAP *bitmap = FreeImage_AllocateT(FIT_RGBAF, 320, 240);
-	if (bitmap) 
-	{
-		FreeImage_Unload(bitmap);
-	}
-	FIBITMAP *bitmap2 = FreeImage_Load(FIF_PNG, "menuNew.png", PNG_DEFAULT);
-	if(bitmap2)
-	{
-		if (FreeImage_Save(FIF_BMP, bitmap2, "menuNew.bmp", BMP_DEFAULT)) 
-		{
-			// bitmap successfully saved!
-		}
-		FreeImage_Unload(bitmap2);
-	}
-	FIBITMAP *bitmap3 = FreeImage_Load(FIF_HDR, "lpbom.hdr", HDR_DEFAULT);
-	if(bitmap3)
-	{
-		if (FreeImage_Save(FIF_PNG, FreeImage_ToneMapping(bitmap3, FITMO_FATTAL02), "lpbom.png", PNG_DEFAULT));
-		{
-			// bitmap successfully saved!
-		}
-		FreeImage_Unload(bitmap3);
-	}
+	std::vector<HdrImage> imagesToInterpolate;
+	imagesToInterpolate.push_back(one);
+	imagesToInterpolate.push_back(two);
+
+	std::vector<int> observations;
+	observations.push_back(2);
+	observations.push_back(4);
+
+	LinearInterpolator li(observations, imagesToInterpolate, 1);
+	HdrImage final = li.calculateInterpolationOn(3);
+	final.saveImage("output.png");
+
 	//std::cout << "Has pixeis " << FreeImage_GetHeight(bitmap) << "\n";
 
 	DLL_API void DLL_CALLCONV FreeImage_DeInitialise();
