cbe029e49526df405afecd35948c264eb9557afd
Thiago Nunes <gorobaum@gmail.com>
2014-11-11 09:01:25 -0200

    FreeImage on linux

--- src/main.cpp
+++ src/main.cpp
@@ -3,27 +3,18 @@
 #include <vector>
 #include <cstring>
 
-#include <opencv2/core/core.hpp>
-#include <opencv2/opencv.hpp>
-#include <opencv2/highgui/highgui.hpp>
-
-#include "interpolation.h"
-#include "imagefunctions.h"
-
-using namespace cv;
-
-#include "hdrfunctions.h"
 #include "FreeImage.h"
 
 int main(int argc, char** argv) 
 {
+	FreeImage_Initialise();
 	DLL_API void DLL_CALLCONV FreeImage_Initialise(BOOL load_local_plugins_only FI_DEFAULT(FALSE));
 	std::cout << FreeImage_GetVersion();
 	FIBITMAP *bitmap = FreeImage_AllocateT(FIT_RGBAF, 320, 240, 32);
 	if (bitmap) 
 	{
-	// bitmap successfully created!
-	FreeImage_Unload(bitmap);
+		// bitmap successfully created!
+		FreeImage_Unload(bitmap);
 	}
 	DLL_API void DLL_CALLCONV FreeImage_DeInitialise();
 	return 0;
