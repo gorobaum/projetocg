b4267f8cffd90a4e070dc8df05dd8b9ee7cb0c1a
Thiago Nunes <gorobaum@gmail.com>
2014-11-11 14:01:05 -0200

    HdrImage

--- src/main.cpp
+++ src/main.cpp
@@ -6,29 +6,14 @@
 #include <FreeImage.h>
 
 #include "gaussinterpolator.h"
+#include "hdrimage.h"
 
 int main(int argc, char** argv) 
 {
 	DLL_API void DLL_CALLCONV FreeImage_Initialise(BOOL load_local_plugins_only FI_DEFAULT(FALSE));
 	std::cout << FreeImage_GetVersion() << "\n";
-	FIBITMAP *bitmap = FreeImage_AllocateT(FIT_RGBAF, 320, 240);
-	if (bitmap) 
-	{
-		FreeImage_Unload(bitmap);
-	}
-	FIBITMAP *bitmap2 = FreeImage_Load(FIF_PNG, "menuNew.png", PNG_DEFAULT);
-	if(bitmap2)
-	{
-		FreeImage_Unload(bitmap2);
-	}
-	FIBITMAP *bitmap3 = FreeImage_Load(FIF_HDR, "lpbom.hdr", HDR_DEFAULT);
-	if(bitmap3)
-	{
-		RGBQUAD value;
-		FreeImage_GetPixelColor(bitmap3, 0, 0, &value);
-		std::cout << "Color in [0,0] = " << value.rgbRed << "\n";
-		FreeImage_Unload(bitmap3);
-	}
+	HdrImage teste("lpbom.hdr");
+	std::cout << "Color in [0,0] = " << teste.getPixelAt(0,0).red << "\n";
 	DLL_API void DLL_CALLCONV FreeImage_DeInitialise();
 	return 0;
 }
